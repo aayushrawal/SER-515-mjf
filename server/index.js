7b231c388c2c6402d46e30fef1266e5ae7ee24a3
Aayush Rawal <aayushrawal@Aayushs-MacBook-Pro.local>
2021-09-30 14:01:46 -0700

    Added mongodb dependencies

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
-const express = require("express");
+const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.get('/api', (req, res) => {
+	res.json({ message: 'Hello from server!' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+	console.log(`Server listening on ${PORT}`);
+});
+
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/newdb';
+
+MongoClient.connect(url, function(err, db) {
+	if (err) throw err;
+	console.log('Database connected!');
+	var dbo = db.db('newdb');
+	dbo.collection('students').insertOne({ name: 'Saurabh', marks: 100 }, function(err, res) {
+		if (err) throw err;
+		console.log('1 document inserted');
+		db.close();
+	});
 });
