69d9d46aad6440eaa99d0a9ebb774156357d12ad
PunithV-1 <vaddipunith002@gmail.com>
2021-09-30 18:00:21 -0700

    Created Registration Form

--- frontend/src/components/Authentication/Registration.js
+++ frontend/src/components/Authentication/Registration.js
@@ -1,42 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 const Registration = (props) => {
-  return(
-        <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+    return (
+        <div className="container">
             <form>
-                <div className="form-group text-left">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" 
-                       className="form-control" 
-                       id="email" 
-                       aria-describedby="emailHelp" 
-                       placeholder="Enter email"
-                />
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                <div className="form-group">
+                    <label for="firstName">First Name</label>
+                    <input type="text" className="form-control" id="firstName" placeholder="Enter First Name" />
                 </div>
-                <div className="form-group text-left">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" 
-                        className="form-control" 
-                        id="password" 
-                        placeholder="Password"
-                    />
+                <div className="form-group">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" className="form-control" id="lastName" placeholder="Enter Last Name" />
                 </div>
-                <div className="form-group text-left">
-                    <label htmlFor="exampleInputPassword1">Confirm Password</label>
-                    <input type="password" 
-                        className="form-control" 
-                        id="confirmPassword" 
-                        placeholder="Confirm Password"
-                    />
+                <div class="form-group">
+                    <label for="userRole">Register As</label>
+                    <select className="form-control" id="userRole">
+                        <option>Select One</option>
+                        <option>Coach</option>
+                        <option>Player</option>
+                    </select>
                 </div>
-                <button 
-                    type="submit" 
-                    className="btn btn-primary"
-                >
-                    Register
-                </button>
+
+                <div className="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" className="form-control" id="email"  placeholder="Enter email" />
+                </div>
+                <div className="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" className="form-control" id="password" placeholder="Enter Password" />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         </div>
+
     )
 }
 
