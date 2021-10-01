a4e3a136913fafb641847045ed53359db9b2115b
krishnasameeth <skris114@asu.edu>
2021-10-01 11:33:55 -0700

    made login api call

--- frontend/src/components/Authentication/Registration.js
+++ frontend/src/components/Authentication/Registration.js
@@ -4,61 +4,66 @@ import { Link } from "react-router-dom";
 
 const Registration = (props) => {
   return (
-    <div className="container">
-      <form>
-        <div className="form-group">
-          <label for="firstName">First Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="firstName"
-            placeholder="Enter First Name"
-          />
-        </div>
-        <div className="form-group">
-          <label for="lastName">Last Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="lastName"
-            placeholder="Enter Last Name"
-          />
-        </div>
-        <div class="form-group">
-          <label for="userRole">Register As</label>
-          <select className="form-control" id="userRole">
-            <option>Select One</option>
-            <option>Coach</option>
-            <option>Player</option>
-          </select>
-        </div>
-
-        <div className="form-group">
-          <label for="email">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="form-group">
-          <label for="password">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Enter Password"
-          />
-        </div>
-        <br />
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
+    <div className="container-fluid">
+      <div className="col-lg-6 col-md-6 col-sm-6">Hello</div>
+      <div className="col-lg-6 col-md-6 col-sm-6">
         <div>
-          <Link to="/login">Back to login</Link>
+          <form>
+            <div className="form-group">
+              <label for="firstName">First Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="firstName"
+                placeholder="Enter First Name"
+              />
+            </div>
+            <div className="form-group">
+              <label for="lastName">Last Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="lastName"
+                placeholder="Enter Last Name"
+              />
+            </div>
+            <div class="form-group">
+              <label for="userRole">Register As</label>
+              <select className="form-control" id="userRole">
+                <option>Select One</option>
+                <option>Coach</option>
+                <option>Player</option>
+              </select>
+            </div>
+
+            <div className="form-group">
+              <label for="email">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group">
+              <label for="password">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Enter Password"
+              />
+            </div>
+            <br />
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+            <div>
+              <Link to="/login">Back to login</Link>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
