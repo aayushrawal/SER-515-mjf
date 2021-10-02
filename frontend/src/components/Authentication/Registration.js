bba3318298711ee600d0d7d91442b957d6d03808
krishnasameeth <skris114@asu.edu>
2021-10-01 18:09:33 -0700

    registration changes

--- frontend/src/components/Authentication/Registration.js
+++ frontend/src/components/Authentication/Registration.js
@@ -1,99 +1,91 @@
 import React, { useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 import { Link } from "react-router-dom";
 
 const Registration = (props) => {
-  const url = "/api/auth/signup"
   const [data, setData] = useState({
-    Firstname: "",
-    Lastname: '',
-    Role: "",
-    Email: "",
-    Password: ""
-  })
-  
-  const handle=(e)=>{
-    const newdata = { ...data }
-    newdata[e.target.id] = e.target.value
-      setData(newdata)
-      console.log(newdata)
-  }
+    username: "",
+    firstName: "",
+    lastName: "",
+    role: "",
+    email: "",
+    password: "",
+  });
 
-  
-  const Signup = (e)=>{
+  const handle = (e) => {
+    const newdata = { ...data };
+    newdata[e.target.id] = e.target.value;
+    setData(newdata);
+  };
+
+  const signup = (e) => {
     e.preventDefault();
-    // (async () => {
-    //   const rawResponse = await fetch("/api/auth/signup", {
-    //     method: "POST",
-    //     headers: {
-    //       Accept: "application/json",
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(data),
-    //   });
-    //   const content = await rawResponse.json();
-    //   setData({
-    //     reset: true,
-    //   });
-    //   console.log(content);
-    // })();
-    axios.post(url, {
-      Firstname: data.Firstname,
-      Lastname: data.Lastname,
-      UserID: data.UserID,
-      Role: data.Role,
-      Email: data.Email,
-      Password: data.Password,
-    },
-    {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    })
-    .then(res => {
-      console.log(res.data)
-    })
-  }
+    (async () => {
+      const rawResponse = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (rawResponse.ok) {
+        console.log("HELLO");
+      }
+      const content = await rawResponse.json();
+
+      setData({
+        reset: true,
+      });
+      console.log(content);
+    })();
+  };
 
   return (
     <div className="container">
-      <form onSubmit={Signup}>
+      <form onSubmit={signup}>
         <div className="form-group">
-          <label htmlFor="Firstname">First Name</label>
+          <label htmlFor="firstName">First Name</label>
           <input
             onChange={handle}
             type="text"
             className="form-control"
-            id="Firstname"
-            value={data.Firstname}
+            id="firstName"
+            value={data.firstName}
             placeholder="Enter First Name"
           />
         </div>
         <div className="form-group">
-          <label htmlFor="Lastname">Last Name</label>
-          <input onChange={handle}
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            onChange={handle}
             type="text"
             className="form-control"
-            id="Lastname"
-            value={data.Lastname}
+            id="lastName"
+            value={data.lastName}
             placeholder="Enter Last Name"
           />
         </div>
         <div className="form-group">
-          <label htmlFor="UserID">User ID</label>
-          <input onChange={handle}
+          <label htmlFor="username"></label>
+          <input
+            onChange={handle}
             type="text"
             className="form-control"
-            id="UserID"
-            value={data.UserID}
-            placeholder="Enter User ID"
+            id="userId"
+            value={data.username}
+            placeholder="Enter username"
           />
         </div>
         <div className="form-group">
-          <label htmlFor="Role">Register As</label>
-          <select className="form-control" id="Role" value={data.Role} onChange={handle}>
+          <label htmlFor="role">Register As</label>
+          <select
+            className="form-control"
+            id="role"
+            value={data.role}
+            onChange={handle}
+          >
             <option>Select One</option>
             <option>Coach</option>
             <option>Player</option>
@@ -101,27 +93,29 @@ const Registration = (props) => {
         </div>
 
         <div className="form-group">
-          <label htmlFor="Email">Email address</label>
-          <input onChange={handle}
+          <label htmlFor="email">Email address</label>
+          <input
+            onChange={handle}
             type="email"
             className="form-control"
-            id="Email"
-            value={data.Email}
+            id="email"
+            value={data.email}
             placeholder="Enter email"
           />
         </div>
         <div className="form-group">
-          <label htmlFor="Password">Password</label>
-          <input onChange={handle}
+          <label htmlFor="password">Password</label>
+          <input
+            onChange={handle}
             type="password"
             className="form-control"
-            id="Password"
-            value={data.Password}
+            id="password"
+            value={data.password}
             placeholder="Enter Password"
           />
         </div>
         <br />
-        <button type="submit" className="btn btn-primary" >
+        <button type="submit" className="btn btn-primary">
           Submit
         </button>
         <div>
