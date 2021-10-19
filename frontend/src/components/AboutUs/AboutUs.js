c38762e542d846193e9202429eef47ddc9394db1
DHRUV PATEL <45648070+dhruv1462@users.noreply.github.com>
2021-10-19 16:52:04 -0700

    Dhruv: US#37 Task#38 Added intial page for about us.

---
+++ frontend/src/components/AboutUs/AboutUs.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./AboutUs.css";
+
+const AboutUs = () => {
+  return (
+    <div class="et_builder_inner_content et_pb_gutters3"><div class="et_pb_section et_pb_section_0 et_section_regular">
+<div class="et_pb_row et_pb_row_0">
+<div class="et_pb_column et_pb_column_4_4 et_pb_column_0  et_pb_css_mix_blend_mode_passthrough et-last-child">
+<div class="et_pb_module et_pb_post_title et_pb_post_title_0 et_pb_bg_layout_light  et_pb_text_align_center">
+<div class="et_pb_title_container">
+<h1 class="entry-title">About Us</h1>
+</div>
+</div><div class="et_pb_module et_pb_text et_pb_text_0  et_pb_text_align_left et_pb_bg_layout_light">
+<div class="et_pb_text_inner"><p><i>Sparky Soccer Association</i>&nbsp;is recognized as the largest soccer league and tournament host in the United States. &nbsp;Our league and tournaments are held at the world class all turf soccer facilities of<span class="scheels-formatted">&nbsp;SCHEELS&nbsp;</span>Overland Park Soccer Complex, Swope Soccer Village, Compass Minerals Sporting Fields and&nbsp;<strong>GARMIN</strong>&nbsp;Olathe Soccer Complex.</p>
+<p>The league accepts players of all skill levels including recreational and premier level players and teams. Each year over 170+ different soccer clubs compete through the&nbsp;<em>Sparky League in Spring, Fall and Winter leagues</em>. &nbsp;Over 1,300+ teams and 30,000+ youth from age 9 to 19 compete each league season and many age groups have up to 9 subdivisions with 9 or more teams playing in each subdivision. The sheer number of teams allow players and teams the opportunity to compete against similarly skilled teams at all ages.</p>
+<p>In addition to league play,&nbsp;<em>Sparky Soccer Association</em>&nbsp;hosts 14+ exceptional tournaments each year. Our tournaments draw in more than 2,700+ teams and 43,000+ youth players, from over 16 states including Texas, Minnesota, Louisiana, Oklahoma, Nebraska, Iowa, South Dakota, Wisconsin, Colorado and Illinois, as well as Mexico and Canada.</p>
+<p>In league and tournaments in addition to the players, there are 4,000 coaches and 2,300 referees each year who participate.</p>
+<p><strong>History</strong></p>
+<p>Founded in 1977 as the Johnson County Soccer League to promote youth soccer in Johnson County, Kansas. &nbsp; 600 players were initially split geographically according to school districts with the Shawnee Mission School District split, due to its size.</p>
+<p>The Kansas Premier Soccer League (KPSL) was established a few years later for players who were ready to expand to competitive soccer and in 1995, KPSL joined Johnson County Soccer as a member club and both the leagues became&nbsp;<em>Sparky Soccer Association</em>.</p>
+<p>Today,&nbsp;<em>Sparky Soccer Association</em>&nbsp;is a nonprofit and educational youth soccer association and has grown through the years to be the largest and most competitive youth soccer league in the United States.</p>
+<p><strong>Play At Sparky</strong></p>
+<p>Participating in the Sparky Soccer league or tournaments is easy. &nbsp;Teams of all skill levels are welcome for both recreational and competitive play. &nbsp;Simply register online for an upcoming event. League and Tournament information can be found under the tabs on this website.</p></div>
+</div> 
+</div> 
+</div> 
+</div>  </div>
+  );
+};
+
+export default AboutUs;
