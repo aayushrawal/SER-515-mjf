023683e0854c5025a91ab430f8c94db2266e3cb9
krishnasameeth <skris114@asu.edu>
2021-10-19 16:52:22 -0700

    Sameet: Refactored the App.js page for routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
-
-import "./App.css";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import Login from "./components/Authentication/Login";
 import Registration from "./components/Authentication/Registration";
@@ -15,22 +8,20 @@ import Home from "./components/Home";
 function App() {
   return (
     <div>
-      <Router>
+      <BrowserRouter>
         <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Registration />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
+          <Route path="/login" exact render={(props) => <Login {...props} />} />
+          <Route
+            path="/register"
+            exact
+            render={(props) => <Registration {...props} />}
+          />
+          <Route path="/home" exact render={(props) => <Home {...props} />} />
           <Route exact path="/">
             <Redirect to="/login" />
           </Route>
         </Switch>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
