c693c83484dc9c3d12636ee9ea9371c799d9dd69
PunithV-1 <vaddipunith002@gmail.com>
2021-10-30 12:24:18 -0700

    Punith: Task #70 Created API for Hotels page

---
+++ server/app/routes/referee.routes.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+    const referee = require("../controllers/referee.controller.js")
+
+    const router = require("express").Router();
+
+    // register a referee
+    router.post("/registration", referee.create);
+
+    app.use("/api/referee", router);
+};
