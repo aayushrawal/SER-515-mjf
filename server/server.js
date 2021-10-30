6f91a4db01f94414e980060bbb23b4fde8f09168
krishnasameeth <skris114@asu.edu>
2021-10-29 17:57:24 -0700

    Sameeth: Task#68 Added user model and connect method

--- server/server.js
+++ server/server.js
@@ -13,6 +13,20 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the databse!", err);
+    process.exit();
+  });
+
 app.get("/", (req, res, next) => {
   res.json({ message: "Welcome to sparky's soccer tournament" });
 });
