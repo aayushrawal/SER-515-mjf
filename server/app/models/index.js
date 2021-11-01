f78d094551042714e8b82fcd41485aec29551fd3
Saurabh Rane <ssrane@asu.edu>
2021-10-31 18:04:48 -0700

    Saurabh Rane Task#61 Create Hotels API to save pusers name and email

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -7,5 +7,6 @@ const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
 db.users = require("./user.model.js")(mongoose);
+db.hotels = require("./hotel.model.js")(mongoose);
 
 module.exports = db;
