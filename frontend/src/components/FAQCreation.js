cfa44350ed4e057682aecfb9570ad18ca6f290a9
DHRUV PATEL <45648070+dhruv1462@users.noreply.github.com>
2021-11-08 11:50:36 -0700

    US#84 Task#112: Create a tournament director FAQ page

---
+++ frontend/src/components/FAQCreation.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import './FAQCreation.scss';
+
+import { Button, Card, CardBody, FormGroup, Input, InputGroupAddon, InputGroupText,
+          InputGroup, Container, Row, Col } from "reactstrap";
+          
+function FAQCreation() {
+	return (
+		<div useRef="main">
+			<div className="section section-shaped section-lg">
+				<div className="shape shape-style-1 bg-gradient-default">
+					<span />
+					<span />
+					<span />
+					<span />
+					<span />
+				</div>
+                <section className="section section-lg bg-gradient-default">
+            <Container className="pt-lg pb-300">
+              <Row className="text-center justify-content-center">
+                <Col lg="10">
+                  <h2 className="display-3 text-white">Enter FAQ</h2>
+                  <p className="lead text-white">
+                    Answers to all question so that particpants will know about the event!!
+                  </p>
+                </Col>
+              </Row>
+                          </Container>    
+          </section>
+                <section className="section section-lg pt-lg-0 section-contact-us">
+            <Container>
+              <Row className="justify-content-center mt--300">
+                <Col lg="8">
+                  <Card className="bg-gradient-secondary shadow">
+                    <CardBody className="p-lg-5">
+                      <h4 className="mb-1">Enter the question and answer it.</h4>
+                      <p className="mt-0">
+                        This answers and questions will be displayed in FAQ page.
+                      </p>
+                      <FormGroup>
+                        <InputGroup className="input-group-alternative">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder="Enter the question over here"
+                            rows="2"
+                            type="textarea"
+                          />
+                        </InputGroup>
+                      </FormGroup>
+                      <FormGroup className="mb-4">
+                        <Input
+                          className="form-control-alternative"
+                          cols="80"
+                          name="name"
+                          placeholder="Type your Answer..."
+                          rows="4"
+                          type="textarea"
+                        />
+                      </FormGroup>
+                      <div>
+                        <Button
+                          block
+                          className="btn-round"
+                          color="default"
+                          size="lg"
+                          type="button"
+                        >
+                          Add FAQ
+                        </Button>
+                      </div>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+          <div className="separator separator-bottom separator-skew zindex-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                preserveAspectRatio="none"
+                version="1.1"
+                viewBox="0 0 2560 100"
+                x="0"
+                y="0"
+              >
+                <polygon
+                  className="fill-white"
+                  points="2560 0 2560 100 0 100"
+                />
+              </svg>
+            </div>
+			</div>
+		</div>
+	);
+}
+
+export default FAQCreation;
