b718c180c16a84b3ed98104c4202d5bb14cbab79
krishnasameeth <skris114@asu.edu>
2021-11-09 12:00:01 -0700

    Sameet US#86 Task#116 & Task#117 added form to sponsor creation page and link in director home page

--- frontend/src/components/SponsorCreation.js
+++ frontend/src/components/SponsorCreation.js
@@ -1,10 +1,111 @@
 import "./SponsorCreation.scss";
-import React from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
 
 const SponsorCreation = () => {
+  const [data, setData] = useState({
+    sponsorName: "",
+  });
+
+  const handle = (e) => {
+    const newdata = { ...data };
+    newdata[e.target.id] = e.target.value;
+    setData(newdata);
+  };
+
+  const addSponsor = () => {
+    console.log("Added sponsor");
+  };
+
   return (
-    <div>
-      <h1>This is the sponsor creation page!</h1>
+    <div className="section section-shaped section-lg">
+      <div className="shape shape-style-1 bg-gradient-default">
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+      </div>
+      <section className=" bg-gradient-default">
+        <Container className="pt-lg pb-300">
+          <Row className="text-center justify-content-center">
+            <Col lg="10">
+              <h2 className="display-3 text-white">Add Tournament Sponsors</h2>
+              <br />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className="section section-lg pt-lg-0 section-contact-us">
+        <Container>
+          <Row className="justify-content-center mt--300">
+            <Col lg="8">
+              <Card className="bg-gradient-secondary shadow">
+                <CardBody className="p-lg-3">
+                  <h4 className="mb-1">
+                    Please add the sponsor name and upload its image.
+                  </h4>
+                  <br />
+                  <Form role="form" onSubmit={addSponsor}>
+                    <FormGroup>
+                      <InputGroup className="input-group-alternative mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="fa fa-money"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                          placeholder="Sponsor name"
+                          type="text"
+                          id="sponsorName"
+                          onChange={handle}
+                          value={data.sponsorName}
+                        />
+                      </InputGroup>
+                    </FormGroup>
+                    <div>
+                      <Button
+                        block
+                        className="btn-round"
+                        color="default"
+                        size="lg"
+                        type="button"
+                      >
+                        Add Sponsor
+                      </Button>
+                    </div>
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <div className="separator separator-bottom separator-skew zindex-100">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+          version="1.1"
+          viewBox="0 0 2560 100"
+          x="0"
+          y="0"
+        >
+          <polygon className="fill-white" points="2560 0 2560 100 0 100" />
+        </svg>
+      </div>
     </div>
   );
 };
--- frontend/src/components/TournamentDirectorHome.js
+++ frontend/src/components/TournamentDirectorHome.js
@@ -6,69 +6,78 @@ import { Button, NavbarBrand } from "reactstrap";
 import { Link } from "react-router-dom";
 
 const TournamentDirectorHome = () => {
-    const EditFAQ = (e) => {
-        e.preventDefault();
-    }
-    const ManageHotel = (e) => {
-        e.preventDefault();
-    }
-    const ManageSponsors = (e) => {
-        e.preventDefault();
-    }
-    return (
-        <section className="section section-lg section-shaped main-section">
-            <div className="shape shape-style-1 bg-gradient-default">
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
+  const EditFAQ = (e) => {
+    e.preventDefault();
+  };
+  const ManageHotel = (e) => {
+    e.preventDefault();
+  };
+  const ManageSponsors = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="section section-lg section-shaped main-section">
+      <div className="shape shape-style-1 bg-gradient-default">
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+      </div>
+      <div className="tournamentdirectorhome">
+        <div className="tournamentdirectorhome-Body">
+          <div className="col-12">
+            <div className="text-center">
+              <NavbarBrand to="/tournament-director/faq-creation" tag={Link}>
+                <Button
+                  onclick={EditFAQ}
+                  className="mt-4"
+                  color="info"
+                  type="submit"
+                  class="btn btn-default"
+                >
+                  Edit FAQ's Page
+                </Button>
+              </NavbarBrand>
             </div>
-            <div className="tournamentdirectorhome">
-                <div className="tournamentdirectorhome-Body">
-                    <div className="col-12">
-                        <div className="text-center">
-                            <NavbarBrand to="/tournament-director/faq-creation" tag={Link}>
-                                <Button onclick={EditFAQ}
-                                    className="mt-4"
-                                    color="info"
-                                    type="submit"
-                                    class="btn btn-default"
-                                >Edit FAQ's Page
-                                </Button>
-                            </NavbarBrand>
-                        </div>
-                        <div className="text-center">
-                            <NavbarBrand to="/" tag={Link}>
-                                <Button onclick={ManageHotel}
-                                    className="mt-4"
-                                    color="info"
-                                    type="submit"
-                                    class="btn btn-default"
-                                >Manage Hotels Page
-                                </Button>
-                            </NavbarBrand>
-                        </div>
-                        <div className="text-center">
-                            <NavbarBrand to="/" tag={Link}>
-                                <Button onclick={ManageSponsors}
-                                    className="mt-4"
-                                    color="info"
-                                    type="submit"
-                                    class="btn btn-default"
-                                >Manage Sponsors Page
-                                </Button>
-                            </NavbarBrand>
-                        </div>
-                    </div>
-                </div>
+            <div className="text-center">
+              <NavbarBrand to="/" tag={Link}>
+                <Button
+                  onclick={ManageHotel}
+                  className="mt-4"
+                  color="info"
+                  type="submit"
+                  class="btn btn-default"
+                >
+                  Manage Hotels Page
+                </Button>
+              </NavbarBrand>
             </div>
-        </section>
-    );
+            <div className="text-center">
+              <NavbarBrand
+                to="/tournament-director/sponsor-creation"
+                tag={Link}
+              >
+                <Button
+                  onclick={ManageSponsors}
+                  className="mt-4"
+                  color="info"
+                  type="submit"
+                  class="btn btn-default"
+                >
+                  Manage Sponsors Page
+                </Button>
+              </NavbarBrand>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TournamentDirectorHome;
