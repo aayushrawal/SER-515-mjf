70d0b6698c86d9763826e9556a12b8d615705154
krishnasameeth <skris114@asu.edu>
2021-11-09 11:41:11 -0700

    Sameet US#86 Task#116 & Task#117 Created the initial sponsor creation page and added route

---
+++ frontend/src/components/SponsorCreation.js
@@ -0,0 +1,12 @@
+import "./SponsorCreation.scss";
+import React from "react";
+
+const SponsorCreation = () => {
+  return (
+    <div>
+      <h1>This is the sponsor creation page!</h1>
+    </div>
+  );
+};
+
+export default SponsorCreation;
