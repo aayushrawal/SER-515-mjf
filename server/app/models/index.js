701339f54473407ce4944a9dfee1a98973747677
DHRUV PATEL <45648070+dhruv1462@users.noreply.github.com>
2021-11-09 21:27:41 -0700

    Create FAQ API for tournamnet director

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -9,5 +9,6 @@ db.url = dbConfig.url;
 db.users = require("./user.model.js")(mongoose);
 db.referee = require("./referee.model.js")(mongoose);
 db.hotels = require("./hotel.model.js")(mongoose);
+db.faqs = require("./faq.director.model.js")(mongoose);
 
 module.exports = db;
