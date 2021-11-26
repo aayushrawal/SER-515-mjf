09e13054e10d65a6190b96b71f42fa3bb7ef6ea9
krishnasameeth <skris114@asu.edu>
2021-11-26 12:08:59 -0700

    US#87 Task#146 updated referee model to add referee status

--- server/app/controllers/referee.controller.js
+++ server/app/controllers/referee.controller.js
@@ -3,57 +3,58 @@ const Referee = db.referee;
 
 // create and save a Referee
 exports.create = (req, res) => {
-    // Validate request for correct input data
-    if (!req.body.refereeName) {
-        res.status(400).send({ message: "referee name missing." });
-        return;
-    }
+  // Validate request for correct input data
+  if (!req.body.refereeName) {
+    res.status(400).send({ message: "referee name missing." });
+    return;
+  }
 
-    if (!req.body.refereeEmail) {
-        res.status(400).send({ message: "referee email missing." });
-        return;
-    }
+  if (!req.body.refereeEmail) {
+    res.status(400).send({ message: "referee email missing." });
+    return;
+  }
 
-    if (!req.body.refereeUsername) {
-        res.status(400).send({ message: "referee username missing." });
-        return;
-    }
+  if (!req.body.refereeUsername) {
+    res.status(400).send({ message: "referee username missing." });
+    return;
+  }
 
-    if (!req.body.refereePhonenumber) {
-        res.status(400).send({ message: "referee phonenumber missing." });
-        return;
-    }
+  if (!req.body.refereePhonenumber) {
+    res.status(400).send({ message: "referee phonenumber missing." });
+    return;
+  }
 
-    if (!req.body.refereeDob) {
-        res.status(400).send({ message: "referee DOB missing." });
-        return;
-    }
+  if (!req.body.refereeDob) {
+    res.status(400).send({ message: "referee DOB missing." });
+    return;
+  }
 
-    if (!req.body.refereeEventcategory) {
-        res.status(400).send({ message: "referee event category missing." });
-        return;
-    }
+  if (!req.body.refereeEventcategory) {
+    res.status(400).send({ message: "referee event category missing." });
+    return;
+  }
 
+  // Create a referee
+  const referee = new Referee({
+    refereeName: req.body.refereeName,
+    refereeEmail: req.body.refereeEmail,
+    refereeUsername: req.body.refereeUsername,
+    refereePhonenumber: req.body.refereePhonenumber,
+    refereeDob: req.body.refereeDob,
+    refereeEventcategory: req.body.refereeEventcategory,
+    refereeStatus: req.body.refereeStatus,
+  });
 
-    // Create a referee
-    const referee = new Referee({
-        refereeName: req.body.refereeName,
-        refereeEmail: req.body.refereeEmail,
-        refereeUsername: req.body.refereeUsername,
-        refereePhonenumber: req.body.refereePhonenumber,
-        refereeDob: req.body.refereeDob,
-        refereeEventcategory: req.body.refereeEventcategory,
+  // save referee to database
+  referee
+    .save(referee)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Unable to register referee. Some error occured.",
+      });
     });
-
-    // save referee to database
-    referee
-        .save(referee)
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            res.status(500).send({
-                message: err.message || "Unable to register referee. Some error occured.",
-            });
-        });
 };
--- server/app/models/referee.model.js
+++ server/app/models/referee.model.js
@@ -1,17 +1,18 @@
 module.exports = (mongoose) => {
-    const Referee = mongoose.model(
-        "referee",
-        mongoose.Schema(
-            {
-                refereeName: String,
-                refereeEmail: String,
-                refereeUsername: String,
-                refereePhonenumber: String,
-                refereeDob: Date,
-                refereeEventcategory: String,
-            },
-            { timestamps: true }
-        )
-    );
-    return Referee;
+  const Referee = mongoose.model(
+    "referee",
+    mongoose.Schema(
+      {
+        refereeName: String,
+        refereeEmail: String,
+        refereeUsername: String,
+        refereePhonenumber: String,
+        refereeDob: Date,
+        refereeEventcategory: String,
+        refereeStatus: String,
+      },
+      { timestamps: true }
+    )
+  );
+  return Referee;
 };
