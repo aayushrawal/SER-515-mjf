6ead49d4f665783675d43e4e74a9c67fedf191ec
krishnasameeth <skris114@asu.edu>
2021-11-26 12:06:52 -0700

    US#87 Task#146 Created a new component for referee applications

---
+++ frontend/src/components/RefereeApplications.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./RefereeApplications.scss";
+
+const RefereeApplications = () => {
+  return (
+    <section className="section section-lg section-shaped main-section">
+      <div className="shape shape-style-1 bg-gradient-default">
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+      </div>
+    </section>
+  );
+};
+
+export default RefereeApplications;
